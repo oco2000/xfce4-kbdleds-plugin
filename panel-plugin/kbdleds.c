b2cc0a84e06ef0938f5db5f41bedcb50d5fb2458
Christopher Head <chead@chead.ca>
2021-02-27 10:57:58 -0800

    Change some includes
    Some macros have now been moved to separate header files. Including
    `libxfce4panel.h` includes all the subordinate header files, ensuring
    that the macros are available.

--- panel-plugin/kbdleds.c
+++ panel-plugin/kbdleds.c
@@ -28,7 +28,7 @@
 
 #include <gtk/gtk.h>
 #include <libxfce4util/libxfce4util.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 
 #include "kbdleds.h"
 #include "kbdleds-dialogs.h"
